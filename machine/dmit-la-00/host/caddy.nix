090901407a2cf6838cf45f7fe7e5734f0fff4f13
barnard33-233 <htang0203@gmail.com>
2026-07-18 04:48:24 +0800

    Revert "add forwardproxy plugin"
    This reverts commit cdce48c37e2d9c77bbda6ad27d1a8d3de6bbc9d1.

--- machine/dmit-la-00/host/caddy.nix
+++ machine/dmit-la-00/host/caddy.nix
@@ -1,13 +1,6 @@
-{lib, pkgs, sld,...}:{
+{sld,...}:{
   services.caddy = {
     enable = true;
-    package = pkgs.caddy.withPlugins {
-      plugins = [
-        "github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive"
-      ];
-      hash = "sha256-fXIDf1kYEKPGjQZ4gtWWosdqCHIMHTs3rvXhEOjybfQ=";
-      doInstallCheck = false;
-    };
     virtualHosts = {
       "bin.${sld}" = {
         extraConfig = ''
