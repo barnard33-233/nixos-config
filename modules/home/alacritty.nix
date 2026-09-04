628a30cfc091927ed4d5c0c42aa7c9ff50be53cf
barnard33-233 <htang0203@gmail.com>
2026-09-04 19:35:17 +0800

    modules/home/alacritty: support specifying [terminal.shell]

--- modules/home/alacritty.nix
+++ modules/home/alacritty.nix
@@ -1,20 +1,40 @@
 {lib, config, pkgs, ...}:
 let
   cfg = config.customHome.alacritty;
+
+  # Render a string as a TOML basic string (escapes backslash and double quote).
+  toTomlStr = s: ''"${lib.replaceStrings [ "\\" "\"" ] [ "\\\\" "\\\"" ] s}"'';
+
+  baseConfig = builtins.readFile ./alacritty/alacritty.toml;
+  shellConfig = ''
+    [terminal.shell]
+    program = ${toTomlStr cfg.shell}
+  '' + lib.optionalString (cfg.shellArgs != []) ''
+    args = [ ${lib.concatMapStringsSep ", " toTomlStr cfg.shellArgs} ]
+  '';
 in
 {
   options.customHome.alacritty = {
     enable = lib.mkEnableOption "Alacritty Config.";
     defaultTerminal = lib.mkEnableOption "Set Alacritty as the default Terminal.";
+    shell = lib.mkOption {
+      type = lib.types.str;
+      default = "bash";
+      description = "Shell program launched by Alacritty.";
+    };
+    shellArgs = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+      description = "Arguments passed to the shell program.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.alacritty ];
-    xdg.configFile."alacritty" = {
-      enable = true;
-      recursive = true;
-      source = ./alacritty;
-    };
+    xdg.configFile."alacritty/alacritty.toml".text = ''
+      ${baseConfig}
+      ${shellConfig}
+    '';
     home.activation.setDefaultTerminal = lib.mkIf cfg.defaultTerminal (lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       ${pkgs.glib}/bin/gsettings set org.gnome.desktop.default-applications.terminal exec alacritty
       ${pkgs.glib}/bin/gsettings set org.gnome.desktop.default-applications.terminal exec-arg -e
